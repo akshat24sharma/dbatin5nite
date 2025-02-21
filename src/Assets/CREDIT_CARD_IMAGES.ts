e0cc83229587e8d705b324989a1cfad87524f228
Akshat Sharma <fc20294@Axis-FC-A00287.local>
2025-02-22 00:39:27 +0530

    image updated for cc carousel

--- src/Assets/CREDIT_CARD_IMAGES.ts
+++ src/Assets/CREDIT_CARD_IMAGES.ts
@@ -1,19 +1,19 @@
 const CREADIT_CARDCAROUSEL_1_IMAGE = [
   {
-    src: new URL('~/src/AssetFiles/ccCarousel1.png?as=avif', import.meta.url)
+    src: new URL('~/src/AssetFiles/ccCarouselUp.png?as=avif', import.meta.url)
       .href,
-    alt: 'ccCarousel1 Image',
+    alt: 'ccCarouselUp Image',
     as: 'image/avif'
   },
   {
-    src: new URL('~/src/AssetFiles/ccCarousel1.png?as=webp', import.meta.url)
+    src: new URL('~/src/AssetFiles/ccCarouselUp.png?as=webp', import.meta.url)
       .href,
-    alt: 'ccCarousel1 Image',
+    alt: 'ccCarouselUp Image',
     as: 'image/webp'
   },
   {
-    src: new URL('~/src/AssetFiles/ccCarousel1.png', import.meta.url).href,
-    alt: 'ccCarousel1 Image',
+    src: new URL('~/src/AssetFiles/ccCarouselUp.png', import.meta.url).href,
+    alt: 'ccCarouselUp Image',
     as: 'image/png'
   }
 ]
