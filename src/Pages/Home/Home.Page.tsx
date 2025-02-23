3dc863786167375334f0b6bd9695094903f444dc
Akshat Sharma <fc20294@Axis-FC-A00287.local>
2025-02-23 19:29:19 +0530

    changes

--- src/Pages/Home/Home.Page.tsx
+++ src/Pages/Home/Home.Page.tsx
@@ -2,7 +2,7 @@ import {
   FC
   // useState
 } from 'react'
-import LendingPage from '../Dev/LendingPage'
+// import LendingPage from '../Dev/LendingPage'
 
 // import CreditCardPage from '../Dev/CreditCardPage'
 // import { ForexPage } from '../Dev/ForexPages'
@@ -13,7 +13,7 @@ import LendingPage from '../Dev/LendingPage'
 const HomePage: FC = () => {
   // const [tabValue, setTabValue] = useState<number>(0)
   // const [openBottomSheet, setOpenBottomSheet] = useState(false)
-  return <LendingPage />
+  return <></>
 }
 
 export default HomePage
